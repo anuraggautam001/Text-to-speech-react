6a2c2e65ab29482138f2d6d605649d76a8ff7545
Anurag Gautam <anuraggautam876@gmail.com>
2023-03-31 00:58:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useSpeechSynthesis } from 'react-speech-kit';
+// import './App.css';
 
-function App() {
+const App = () =>{
+  
+  const { speak } = useSpeechSynthesis();
+
+  const [value, setValue] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+       <div className='speech'>
+            <div className='group'>
+              <h2>Text to Speech Converter Using React</h2>
+            </div>
+            <div className='group'>
+              <textarea rows={10} onChange={(e) => setValue(e.target.value)}></textarea>
+            </div>
+            <div className='group'>
+              <button onClick={() => speak({text:value})}>
+                Speech
+              </button>
+            </div>
+       </div>
+    </>
+  )
+};
 
 export default App;
