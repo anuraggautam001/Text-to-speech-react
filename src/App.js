2eda0b5f44a319e035df59751da39cf77f963bf4
Anurag Gautam <anuraggautam876@gmail.com>
2023-04-02 22:04:56 +0530

    Enhanced

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ const App = () =>{
   const [value, setValue] = useState("");
   return (
     <>
-       <div className='speech'>
-            <div className='group'>
+       <div className='layout'>
+            <div className='content'>
               <h2>Text to Speech Converter Using React</h2>
             </div>
-            <div className='group'>
+            <div className='content'>
               <textarea rows={2} cols={80} placeholder='Type anything here.... then press speech' onChange={(e) => setValue(e.target.value)}></textarea>
             </div>
-            <div className='group'>
+            <div className='content'>
               <button onClick={() => speak({text:value})}>
                 Speech
               </button>
