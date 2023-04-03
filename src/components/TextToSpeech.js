5c248611064a1547191cc104a1ee9567719928bd
Anurag Gautam <anuraggautam876@gmail.com>
2023-04-03 22:40:58 +0530

    optimized

---
+++ src/components/TextToSpeech.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { useSpeechSynthesis } from 'react-speech-kit';
+
+
+const TextToSpeech = () =>{
+
+    const { speak } = useSpeechSynthesis();
+
+    const [value, setValue] = useState("");
+
+    return(
+        <div className='layout'>
+            <div className='content'>
+              <h2>Text to Speech Converter Using React</h2>
+            </div>
+            <div className='content'>
+              <textarea rows={2} cols={80} placeholder='Type anything here.... then press speech' 
+                onChange={(e) => setValue(e.target.value)}>
+              </textarea>
+            </div>
+            <div className='content'>
+              <button onClick={() => speak({text:value})}>
+                Speech
+              </button>
+            </div>
+       </div>
+
+    );
+};
+
+export default TextToSpeech;
