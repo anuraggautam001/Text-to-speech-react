7ecf2acd33196db11186b3caeec4a430bf43161a
Anurag Gautam <anuraggautam876@gmail.com>
2023-04-04 22:29:23 +0530

    Component initialised

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () =>{
+    return(
+        <>
+             {/* going to make some layout */}
+        
+        </>
+    );
+
+}
+
+export default Home;
